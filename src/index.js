0e5f9d23600dcf509d46ac4d6eda04cf7261e787
Matt Geller <MattG92@gmail.com>
2017-08-29 18:37:16 -0700

    finished the react into to my satifaction I think

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const luckyNumber = () => Math.floor(Math.random() * 1000 + 1);
+
+const user = {
+    name: "Everyman",
+    luckyNum: luckyNumber()
+};
+
+const greeting = (userObj) => (
+    <div className="container">
+        <h1>Hello, {userObj.name}</h1>
+        <h2 className="text-muted">Your lucky number is {userObj.luckyNum}</h2>
+    </div>
+);
+
+
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
