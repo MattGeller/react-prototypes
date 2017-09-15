8801af258928e5c7408acc7b325570d43773b814
Matt Geller <MattG92@gmail.com>
2017-09-15 14:38:38 -0700

    working on the stopwatch

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,12 @@
+import React from 'react';
+import Stopwatch from './stopwatch';
 
+const App = () => (
+    <div className="container">
+        <h1 className="text-center"><em>Stopwatch</em></h1>
+        <Stopwatch/>
+    </div>
+);
+
+export default App;
 
